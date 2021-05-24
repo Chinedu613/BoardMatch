f349c6e7abbfc3ac6949af44bd77e560cc173a6e
Alex <a.chisar00@gmail.com>
2021-05-24 19:43:37 -0400

    Set-up home page and search page starter code

---
+++ src/components/Pages/Search.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import GameSearchBar  from "../GameSearchBar/index";
+
+function Search() {
+    return (
+        <div>
+            <GameSearchBar/>
+        </div>
+    );
+}
+
+export default Search;
