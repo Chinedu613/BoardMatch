e1e88cfae752d97fe76c5f57c6c3c54546077b39
burtk33 <burtk33@gmail.com>
2021-05-26 18:47:41 -0400

    editing file structure for best practices

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import React, {useState} from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 import Header from './components/Header/Header.js';
 import Home from "./components/Pages/Home";
 import Search from "./components/Pages/Search";
@@ -7,13 +7,18 @@ import ModalRoot from "./modules/modals/components/ModalRoot.js";
 import Login from './components/Pages/Login';
 
 function App() {
+  const [auth, setAuth]=useState(false);
+
+
   return (
     <div>
       <ModalRoot />
       <Router>
       <Header />
       <Switch>
-      <Route exact path="/" component={Home}/>
+      <Route exact path="/">
+        {auth ? <Redirect to="/login"/> : <Home/>}
+      </Route>
       <Route exact path="/search" component={Search}/>
       <Route exact path="/login" component={Login}/>
       </Switch>
