c479e85ad0731aea84e926f2177d690f68f3fc25
burtk33 <burtk33@gmail.com>
2021-05-26 19:45:42 -0400

    adding server file

---
+++ server.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const routes = require('./controllers');
+const sequelize = require('./src/config/connection');
+require('dotenv').config();
+
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+  secret: 'secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
