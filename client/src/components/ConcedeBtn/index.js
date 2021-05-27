28826b411ba5dda4e576f48803b770d29e0f7e79
Alex <a.chisar00@gmail.com>
2021-05-26 21:12:12 -0400

    match profile and buttons added

---
+++ client/src/components/ConcedeBtn/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Button } from "react-bootstrap";
+
+export default function concedeBtn() {
+    return (
+        <Button variant="danger">Concede</Button>
+    );
+}
