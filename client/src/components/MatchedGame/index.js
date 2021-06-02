be55eea17cf0268f77bd124599898e532700f525
Latte <cchukwuani@gmail.com>
2021-06-01 21:08:25 -0400

    changes

--- client/src/components/MatchedGame/index.js
+++ client/src/components/MatchedGame/index.js
@@ -5,18 +5,20 @@ import "./style.css";
 export default function MatchedGame() {
     return (
         <Container>
-            <Row>
-                <Col sm={12} className="matched__game-image">
-                    <div>
-                        <img src="https://www.placecage.com/240/160" /> 
-                        </div>
-                </Col>
-                <Col sm={12} className="matched__game-title">
-                    <div>
-                        <h3>Game Title</h3>
-                    </div>
-                </Col>
-            </Row>
+            
+        <Row>
+            <Col className="matched__game-image">
+            <div>
+                <img src="https://www.placecage.com/200/200" /> 
+            </div>
+            </Col>
+        </Row>
+        <Row>
+            <Col className="matched__game-title">
+            <h3>Game Title</h3>
+            </Col>
+        </Row>
+
         </Container>
     )
 }
