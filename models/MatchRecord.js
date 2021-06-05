71de82dcd38459755d84721281096a8bbbb06b97
geotecdev <mikeruanedev@gmail.com>
2021-06-05 11:14:48 -0400

    db and route updates for matches

--- models/MatchRecord.js
+++ models/MatchRecord.js
@@ -1,28 +1,15 @@
-class MatchTransaction {
-    constructor(swipeStatus="") {
-        this.userId;
-        this.userName;
-        this.password;
-        this.lat;
-        this.lon;
-        this.favoriteGame;
-        this.skillLevel;
-        this.email;
-        this.testSize;
-        this.minSkillLevel;
-        this.maxSkillLevel;
-        this.swipeId;
-        this.prospectUserId;
-        this.prospectUserName;
-        this.prospectLat;
-        this.prospectLon;
-        this.distance;
-        this.message;
-        this.status;
+class MatchRecord {
+    constructor() {
+        this.clientUserId;
+        this.matchUserId;
         this.game;
-    }
-
-    setSwipe() {
-
+        this.matchUserName;
+        this.matchUserSkillLevel;
+        this.matchUserLat;
+        this.matchUserLon;
+        this.distance;
+        this.clientMessage;
+        this.matchMessage;
+        this.game;
     }
 }
