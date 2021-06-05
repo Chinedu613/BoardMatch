5d94d981e9c7d5a03e844f1b18a54bdff2b56711
Alex <a.chisar00@gmail.com>
2021-06-04 21:40:58 -0400

    random match

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -12,6 +12,19 @@ router.get('/', async (req,res)=>{
     }
   })
 
+  router.get('/:id', async (req, res) => {
+    try {
+      const userData = await User.findByPk(req.params.id)
+      if (!userData) {
+        res.status(404).json({ message: 'No user found with this id!' });
+        return;
+      }
+      res.status(200).json(userData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
   router.get('/auth', (req,res)=>{
     res.send(req.session.logged_in);
   })
