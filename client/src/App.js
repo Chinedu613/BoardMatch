8cce66fe17d05e8e25a65e90acb031f2cfa3956a
burtk33 <burtk33@gmail.com>
2021-06-07 16:16:50 -0400

    trying to solve deployment issues

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
-import React, {useState, useEffect} from 'react';
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header/Header.js';
-import Home from "./Pages/Home";
-import Search from "./Pages/Search";
-import Login from './Pages/Login';
-import Game from './Pages/Game';
-import SignUp from './Pages/SignUp';
+import Home from "./pages/Home";
+import Search from "./pages/Search";
+import Login from './pages/Login';
+import Game from './pages/Game';
+import SignUp from './pages/SignUp';
 import LogHeader from "./components/LogHeader";
 import Users from "./utils/Users";
 import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
@@ -13,32 +13,35 @@ import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
 const queryClient = new QueryClient()
 
 function App() {
-  const [authorization, setAuth]= useState(false);
+  const [authorization, setAuth] = useState(false);
 
-  useEffect(()=>{
-    Users.auth().then(res=>{
+  useEffect(() => {
+    Users.auth().then(res => {
       console.log(res);
       setAuth(res.data);
     })
   })
 
   return (
-    <div>
-      <QueryClientProvider client={queryClient}>
-      <Router>
-      {authorization ? <LogHeader /> : <Header/>}
-      <Switch>
-      <Route exact path="/">
-         <Home/>
-      </Route>
-      <Route exact path="/search" component={Search}/>
-      <Route exact path="/login" component={Login}/>
-      <Route exact path="/signup" component={SignUp}/>
-      <Route exact path="/game" component={Game}/>
-      </Switch>
-      </Router>
-      </QueryClientProvider>
-    </div>
+    <Router>
+      <div>
+        <QueryClientProvider client={queryClient}>
+
+          {authorization ? <LogHeader /> : <Header />}
+          <Switch>
+            <Route exact path="/search" component={Search} />
+            <Route exact path="/login">
+              <Login />
+            </Route>
+            <Route exact path="/signup" component={SignUp} />
+            <Route exact path="/game" component={Game} />
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </QueryClientProvider>
+      </div>
+    </Router>
   );
 }
 
